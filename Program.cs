abfa8d818eb2126efb2aeacaeb3f3805d5dd16a9
LautaroSuppa <lautarosuppa@gmail.com>
2026-05-10 10:54:57 -0300

    no hice nada

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public class Tarea {
+    public int TareaID { get; set; }
+    public string Descripcion { get; set; }
+    public int Duracion { get; set; } 
+}
